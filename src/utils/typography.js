64b9bb21f9992117610477641c37e1a883afc629
git01290-wakai <wakai.atsushi@members.co.jp>
2019-12-03 18:14:37 +0900

    修正

---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography"
+import fairyGateTheme from "typography-theme-fairy-gates"
+const typography = new Typography(fairyGateTheme)
+export const { scale, thythm, ooptions } = typography
+export default typography
+export const rhythm = typography.rhythm
